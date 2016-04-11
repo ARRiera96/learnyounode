2f49e706ab5931c5e07a71cb743e7c855c68b09b
ARRiera96 <rriera96@gmail.com>
2016-04-11 14:06:08 -0400

    Update makingItModular.js

--- learnyounode/makingItModular.js
+++ learnyounode/makingItModular.js
@@ -1,8 +1,21 @@
-//myModule.js
-var fs = require('fs')
+
 var mymodule = require('./mymodule.js')
 
 var dir = process.argv[2];
 var ext= process.argv[3]
 var pat = RegExp('\\.' + ext + '$');
 
+var callBack= function(err,list){
+			if(err){
+			console.log(err);
+		}
+	for(var i=0; i< list.length; i++){
+		if(pat.test(list[i])){
+			console.log(list[i]);
+		}
+	}
+
+}
+
+
+mymodule(dir,ext,callBack);
